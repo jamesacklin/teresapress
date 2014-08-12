fda55070ea4dd6de4e34b614b2e306d87052c091
James Acklin <james@acklindesign.com>
2014-08-11 21:51:27 -0400

    Syntax error

--- parts/shared/header.php
+++ parts/shared/header.php
@@ -1,7 +1,7 @@
 <header role="banner">
 	<nav role="navigation">
 		<ul>
-			<?php wp_list_pages(['title_li'=>'']); ?>
+			<?php wp_list_pages('title_li'=>''); ?>
 		</ul>
 	</nav>
 	<h1><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
