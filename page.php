d28d412e05c366f544fc978d806d53f6824b9bba
James Acklin <james@acklindesign.com>
2014-08-11 21:59:29 -0400

    Forgot to wrap the page in article

--- page.php
+++ page.php
@@ -17,10 +17,12 @@
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-	<header>
-		<h2><?php the_title(); ?></h2>
-	</header>
-	<?php the_content(); ?>
+	<article>
+		<header>
+			<h2><?php the_title(); ?></h2>
+		</header>
+		<?php the_content(); ?>
+	</article>
 <?php endwhile; ?>
 
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
