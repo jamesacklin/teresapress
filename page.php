e9b2c9a093430d487436feaec51e406c5e86253c
James Acklin <james@acklindesign.com>
2014-08-11 21:49:30 -0400

    Navigation and no comments on pages

--- page.php
+++ page.php
@@ -17,9 +17,9 @@
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-<h2><?php the_title(); ?></h2>
-<?php the_content(); ?>
-<?php comments_template( '', true ); ?>
+	<header>
+		<h2><?php the_title(); ?></h2>
+	</header>
 <?php endwhile; ?>
 
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
--- parts/shared/header.php
+++ parts/shared/header.php
@@ -1,4 +1,9 @@
 <header role="banner">
+	<nav role="navigation">
+		<ul>
+			<?php wp_list_pages(['title_li'=>'']); ?>
+		</ul>
+	</nav>
 	<h1><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-	<?php bloginfo( 'description' ); ?>
+	<p><?php bloginfo( 'description' ); ?></p>
 </header>
