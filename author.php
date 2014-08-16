9842007e4cde5f347803df23a00838ec5f501cf3
James Acklin <james@acklindesign.com>
2014-08-16 17:23:02 -0400

    Updates to archive / category / tag / search pages

--- author.php
+++ author.php
@@ -21,17 +21,14 @@
 <?php the_author_meta( 'description' ); ?>
 <?php endif; ?>
 
-<ol>
 <?php rewind_posts(); while ( have_posts() ) : the_post(); ?>
-	<li>
-		<article>
+	<article>
+		<header>
 			<h2><a href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-			<time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date(); ?> <?php the_time(); ?></time> <?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?>
-			<?php the_content(); ?>
-		</article>
-	</li>
+		</header>
+		<?php the_content(); ?>
+	</article>
 <?php endwhile; ?>
-</ol>
 
 <?php else: ?>
 <h2>No posts to display for <?php echo get_the_author() ; ?></h2>	
